c8ee9159cd8240ac84b7194eeefb997db4102307
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-18 01:07:15 -0300

    :rocket: Feat: inicia projeto

---
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/SupermercadoApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.supermercado_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermercadoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermercadoApiApplication.class, args);
+	}
+
+}
---
+++ supermercado-api/src/test/java/com/projeto/supermercado_api/SupermercadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.supermercado_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
