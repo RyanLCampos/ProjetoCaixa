a10bbb0c3c1fad0ebb1115778720b798ce84cf26
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-18 02:26:37 -0300

    :sparkles: Feat: implementa entidade Usuario

--- supermercado-api/src/main/java/com/projeto/supermercado_api/model/Usuario.java
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/model/Usuario.java
@@ -1,4 +1,42 @@
 package com.projeto.supermercado_api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Usuario {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
 }
