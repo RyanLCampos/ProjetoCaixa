bf203650f34b5a60dba13c242106299a91fb3ccc
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-18 21:33:41 -0300

    :sparkles: Feat: adiciona método findByEmail no UsuarioRepository

--- supermercado-api/src/main/java/com/projeto/supermercado_api/repository/UsuarioRepository.java
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/repository/UsuarioRepository.java
@@ -3,7 +3,11 @@ package com.projeto.supermercado_api.repository;
 import com.projeto.supermercado_api.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
