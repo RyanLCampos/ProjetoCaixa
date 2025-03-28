ab17117534bb41fb99123586e7f6614093f5b090
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-27 21:23:35 -0300

    :fire: Remove: deleta imports desnecessarios

--- supermercado-api/src/main/java/com/projeto/supermercado_api/controller/cummon/GlobalExceptionHandler.java
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/controller/cummon/GlobalExceptionHandler.java
@@ -1,8 +1,4 @@
 package com.projeto.supermercado_api.controller.cummon;
-
-import com.projeto.supermercado_api.exceptions.RegistroDuplicadoException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
--- supermercado-api/src/main/java/com/projeto/supermercado_api/model/Venda.java
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/model/Venda.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 @Entity
