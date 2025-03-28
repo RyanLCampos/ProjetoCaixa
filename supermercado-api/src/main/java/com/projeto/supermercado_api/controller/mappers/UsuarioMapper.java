67ffb46b9e2d3fcf33615274c6cca58b46f4812d
RyanLCampos <ryanlcampos19@gmail.com>
2025-03-27 21:26:17 -0300

    :bug: Fix: corrige alguns bugs no UsuarioMapper

--- supermercado-api/src/main/java/com/projeto/supermercado_api/controller/mappers/UsuarioMapper.java
+++ supermercado-api/src/main/java/com/projeto/supermercado_api/controller/mappers/UsuarioMapper.java
@@ -2,9 +2,15 @@ package com.projeto.supermercado_api.controller.mappers;
 
 import com.projeto.supermercado_api.controller.dto.UsuarioDTO;
 import com.projeto.supermercado_api.model.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface UsuarioMapper {
 
+    @Mapping(source = "nome", target = "nome")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "senha", target = "senha")
     Usuario toEntity(UsuarioDTO dto);
 
     UsuarioDTO toDTO(Usuario usuario);
